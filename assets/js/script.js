6020763712038dde4db7e0f82026f2cbb593ac4e
dardinierjames <dardinier.james@orange.fr>
2020-11-04 14:35:56 +0100

    enlargement

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+document.getElementById('size').onclick = enlargement;
+
+    function enlargement(){
+        var xxl = document.getElementById('size');
+        xxl.innerHTML = ' <img src="assets/img/nature.jpg" style="width : 600px;"> ';
+
+    }
+
+// document.getElementById('size').ondblclick = decrease;
+//     function decrease(){
+//         var xs = document.getElementById('size');
+//         xs.innerHTML = ' <img src="assets/img/nature.jpg" style="width : 400px;"> ';
+//     }
+
+
+
+
+
+
+
+    
